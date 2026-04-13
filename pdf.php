6d6874ff9eaf996774ee4356c8074bb49b60492a
Abhishek-Leaningcode <abhishekyadav4812@gmail.com>
2026-04-13 12:30:47 +0530

    inital commit

--- pdf.php
+++ pdf.php
@@ -8,9 +8,11 @@ $pdf->setfont("times","I","10");
 $pdf->Cell("0","10","Emp_table","1","1","C");
 $html='<table border="1" cellpadding="10" cellspacing="8" style="background-color:lightbule" >
 <tr>
-<td>Productid</td>
-<td>Productname</td>
-<td>Quantity</td>
+<td>booktitle</td>
+<td>authorname</td>
+<td>genre</td>
+<td>totalcopies</td>
+<td>availablecopies</td>
 </tr>';
 while ($row=$result->fetch_assoc()){
     $html.='<tr>
